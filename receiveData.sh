f9f5667e76b4568e8f24d9338c278b9c903b6760
Philipp Zumstein <zuphilip@gmail.com>
2018-12-17 10:22:34 +0100

    Fix script for receiving the data dump
    Looks like a refactoring did produce this error.

--- receiveData.sh
+++ receiveData.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X GET "https://locdb-analyze-json-dump.bib.uni-mannheim.de/locdb-analyze-json-dump/bibliographicResources" -H  "accept: application/json" -o data.json
+curl -X GET "https://locdb.bib.uni-mannheim.de/locdb/bibliographicResources" -H  "accept: application/json" -o data.json
